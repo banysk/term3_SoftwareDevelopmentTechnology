3714a60c935d443085c209bea1b8c6cc16d8206f
Nikita Basov <49277174+banysk@users.noreply.github.com>
2020-10-29 11:06:40 +1000

    Update ComplexNumber.h

--- ComplexNumber/ComplexNumber.h
+++ ComplexNumber/ComplexNumber.h
@@ -5,8 +5,8 @@
 class ComplexNumber {
 public:
 	ComplexNumber();
-	ComplexNumber(double re, double im = 0);
-	ComplexNumber(RationalNumber re, RationalNumber im);
+	ComplexNumber(const double &re, const double &im = 0);
+	ComplexNumber(const RationalNumber &re, const RationalNumber &im);
 	friend ComplexNumber operator+(const ComplexNumber &left, const ComplexNumber &right);
 	ComplexNumber& operator+=(const ComplexNumber &right);
 	friend ComplexNumber operator-(const ComplexNumber &left, const ComplexNumber &right);
@@ -20,15 +20,15 @@ public:
 	ComplexNumber& operator=(const ComplexNumber &right);
 	ComplexNumber& operator=(const double &right);
 	ComplexNumber operator-();
-	ComplexNumber Pow(int p);
+	ComplexNumber Pow(const int &p);
 	double arg();
 	double abs();
-	RationalNumber getReal();
-	RationalNumber getImagine();
-	void setReal(long long in);
-	void setReal(RationalNumber in);
-	void setImagine(long long in);
-	void setImagine(RationalNumber in);
+	RationalNumber getReal() const;
+	RationalNumber getImagine() const;
+	void setReal(const long long &in);
+	void setReal(const RationalNumber &in);
+	void setImagine(const long long &in);
+	void setImagine(const RationalNumber &in);
 	std::string toString();
 	friend std::ostream& operator<<(std::ostream &out, ComplexNumber &complexnumber);
 private:
